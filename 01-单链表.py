2b2e20a3560b0fec97812004dd90ee5941068734
马治国 <15122302865@163.com>
2017-12-06 15:03:35 +0800

    判空、遍历、长度、尾部添加

--- 01-单链表.py
+++ 01-单链表.py
@@ -1,41 +1,87 @@
 
-class SingleNode(object):
-    """docstring for SingleNode"""
-    def __init__(self, item):
-        #_item存放数据元素
-        self.item = item
-        #_next存放下一个节点的标识
-        self.next = None 
+class Node(object):
+    """docstring for Node"""
+    def __init__(self,elem):
+        #存放节点元素
+        self.elem = elem
+        #存放下一节点标识
+        self.next = None
+        
+        
 
-    #链表是否为空
+
+
+class SingleLinkList(object):
+    """单链表"""
+    def __init__(self,node_tmp = None):
+        #私有属性存放头节点
+        self._head = node_tmp
+ 
+
+    """链表是否为空"""
     def is_empty(self):
-        pass
+        return self._head == None
 
-    #链表长度
+    """链表长度"""
     def length(self):
-        pass
+        #cur 游标，用来移动遍历节点
+        cur = self._head
+        # count 记录数量
+        count = 0
+        while cur != None:
+            count += 1
+            cur = cur.next
+        return count
 
-    #遍历整个链表
+    """遍历整个链表"""
     def travel(self):
-        pass
+        cur = self._head
+        while cur != None:
+            print(cur.elem)
+            cur = cur.next
+
 
     #链表头部添加元素
-    def add(self):
+    def add(self,item_t):
         pass
 
-    #链表尾部添加元素
-    def append(self):
+    """链表尾部添加元素"""
+    def append(self,item_t):
+        node = Node(item_t)
+        if self.is_empty() :
+            self._head = node
+        else:
+            cur = self._head
+            while cur.next != None:
+                cur = cur.next
+            cur.next = node
+
+    """指定位置插入元素"""
+    def insert(self,pos,item_t):
         pass
 
-    #指定位置插入元素
-    def insert(self):
+    """删除节点"""
+    def remove(self,item_t):
         pass
 
-    #删除节点
-    def remove(self):
-        pass
-
-    #查找节点是否存在
+    """查找节点是否存在"""
     def search(self):
         pass
 
+
+def main():
+    ll = SingleLinkList()
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.append(1)
+    print(ll.is_empty())
+    print(ll.length())
+
+    ll.append(2)
+    ll.append(3)
+    ll.append(4)
+    ll.travel()
+
+if __name__ == '__main__':
+    main()
