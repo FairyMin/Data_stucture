02c75d2ca9cb1ea6ec88c4df9ea3358b5a45c49a
马治国 <15122302865@163.com>
2017-12-06 13:33:21 +0800

    框架V0.0

---
+++ 01-单链表.py
@@ -0,0 +1,41 @@
+
+class SingleNode(object):
+    """docstring for SingleNode"""
+    def __init__(self, item):
+        #_item存放数据元素
+        self.item = item
+        #_next存放下一个节点的标识
+        self.next = None 
+
+    #链表是否为空
+    def is_empty(self):
+        pass
+
+    #链表长度
+    def length(self):
+        pass
+
+    #遍历整个链表
+    def travel(self):
+        pass
+
+    #链表头部添加元素
+    def add(self):
+        pass
+
+    #链表尾部添加元素
+    def append(self):
+        pass
+
+    #指定位置插入元素
+    def insert(self):
+        pass
+
+    #删除节点
+    def remove(self):
+        pass
+
+    #查找节点是否存在
+    def search(self):
+        pass
+
